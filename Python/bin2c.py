fde56f92a343082b6c4b53fa47781697c51f1404
Marco Vedovati <marcov@users.noreply.github.com>
2017-06-03 13:01:21 +0200

    Update bin2c.py
    more usable...

--- Python/bin2c.py
+++ Python/bin2c.py
@@ -5,12 +5,15 @@
 # License: GPLv2
 #
 # Usage: bin2c.py <list of files to convert>
-# 
-# You can customize the source code file name by modifying variable 
+#
+# You can customize the source code file name by modifying variable
 # OUTFILE.
 #
 
 import sys
+
+MODE_HEX_STR = False
+WRAP_SIZE = 16
 
 OUTFILE = "output"
 ########################################################################
@@ -22,8 +25,8 @@ def gotoexit(retcode):
   for f in openedfiles:
     f.close()
   sys.exit(retcode)
-  
-########################################################################  
+
+########################################################################
 
 def filename_to_variable(filename):
   retvalue = filename
@@ -35,23 +38,42 @@ def filename_to_variable(filename):
        retvalue = retvalue.replace(c, "_")
   return retvalue
 
-########################################################################  
+########################################################################
 
 def convertsinglefile(f, filename):
   f.seek(0)
-  c = f.read(1)
-  
-  ofh.write("extern const unsigned char * %s; \n" % filename_to_variable(filename))
-  ofc.write("const unsigned char * %s = \"" % filename_to_variable(filename))
-  while c != "" :
-    ofc.write("\\x%02X" % ord(c))
-    c = f.read(1)
+  wrap_ctr = 0
 
-  ofc.write("\";\n")
-  ofc.write("\n")
-  
+  if MODE_HEX_STR:
+    ofh.write("extern const unsigned char * %s; \n" % filename_to_variable(filename))
+    ofc.write("const unsigned char * %s = " % filename_to_variable(filename))
+  else:
+    ofh.write("extern const unsigned char %s[]; \n" % filename_to_variable(filename))
+    ofc.write("const unsigned char %s[] = " % filename_to_variable(filename))
 
-########################################################################  
+  if MODE_HEX_STR:
+    ofc.write("\n    \"")
+  else:
+    ofc.write("{\n    ")
+
+
+  for c in f.read():
+    if MODE_HEX_STR:
+      ofc.write("\\x%02X" % ord(c))
+    else:
+      ofc.write("0x{:02X}, ".format(ord(c)))
+
+    wrap_ctr += 1
+    if wrap_ctr >= WRAP_SIZE:
+        wrap_ctr = 0
+        ofc.write("\n    ")
+
+  if MODE_HEX_STR:
+    ofc.write("\";\n\n")
+  else:
+    ofc.write("};\n\n")
+
+########################################################################
 
 try:
   ofc = open(OUTFILE+".c", "wb")
@@ -68,14 +90,14 @@ if (len(sys.argv) > 1):
   filelist = sys.argv[1:]
   ofc.write("#include \"%s.h\"" % OUTFILE)
   ofc.write("\n\n")
-  
+
   ofh.write("#ifndef __%s_H__" % OUTFILE.upper())
   ofh.write("\n")
   ofh.write("#define __%s_H__" % OUTFILE.upper())
   ofh.write("\n\n")
-  
+
   for filename in filelist:
-    try: 
+    try:
       f = open(filename, "rb")
       openedfiles.append(f)
       print "Converting file %s" % filename
@@ -84,17 +106,8 @@ if (len(sys.argv) > 1):
       openedfiles.remove(f)
     except IOError:
       print "Cannot open %s" % filename
-      gotoexit(-1) 
-    
-  ofh.write("\n#endif")  
-  
+      gotoexit(-1)
+
+  ofh.write("\n#endif")
+
 gotoexit(0)
-  
-
-  
-  
-      
-      
-       
-    
-
